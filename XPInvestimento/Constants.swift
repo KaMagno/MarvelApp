2f3b8f6f39f32dad26e973161fd10a8ebdecbf7b
Kaique Magno <kaique.magno@hotmail.com>
2018-04-21 16:54:33 -0300

    Added Util classes

---
+++ XPInvestimento/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  XPInvestimento
+//
+//  Created by Kaique Magno Dos Santos on 21/04/18.
+//  Copyright © 2018 Kaique Magno. All rights reserved.
+//
+
+import UIKit
+
+final class Constants {
+    
+    final class API {
+        static let privateKey:String = "f971fc5cb921036c6c40587d679b85ad"
+        static let publicKey:String = "f971fc5cb921036c6c40587d679b85ad"
+        static let ts = Date().timeIntervalSince1970.description
+    }
+    
+}
---
+++ XPInvestimento/Logger.swift
@@ -0,0 +1,44 @@
+//
+//  Logger.swift
+//  XPInvestimento
+//
+//  Created by Kaique Magno Dos Santos on 21/04/18.
+//  Copyright © 2018 Kaique Magno. All rights reserved.
+//
+
+import UIKit
+
+final class Logger {
+    
+    static func log(in classBeingLogged:NSObject, function:String=#function, message:String){
+        let customMessage = "\(String(describing:classBeingLogged)) - \(function) - \(message)"
+        print("💬 \(customMessage)")
+    }
+    
+    static func logError(in classBeingLogged:NSObject, function:String=#function, message:String){
+        let customMessage = "\(String(describing:classBeingLogged)) - \(function) - \(message)"
+        let error = NSError(domain: "com.mackmobile.MiniChallenge5", code: 1, userInfo: ["description" : customMessage])
+        print("❌ \(customMessage)")
+    }
+    
+    static func logWarning(in classBeingLogged:NSObject, function:String=#function, message:String){
+        let customMessage = "\(String(describing:classBeingLogged)) - \(function) - \(message)"
+        print("⚠️ \(customMessage)")
+    }
+    
+    static func log(in classBeingLogged:NSObject.Type, function:String=#function, message:String){
+        let customMessage = "\(String(describing:classBeingLogged)) - \(function) - \(message)"
+        print("💬 \(customMessage)")
+    }
+    
+    static func logError(in classBeingLogged:NSObject.Type, function:String=#function, message:String){
+        let customMessage = "\(String(describing:classBeingLogged)) - \(function) - \(message)"
+        let error = NSError(domain: "com.mackmobile.MiniChallenge5", code: 1, userInfo: ["description" : message])
+        print("❌ \(customMessage)")
+    }
+    
+    static func logWarning(in classBeingLogged:NSObject.Type, function:String=#function, message:String){
+        let customMessage = "\(String(describing:classBeingLogged)) - \(function) - \(message)"
+        print("⚠️ \(customMessage)")
+    }
+}
