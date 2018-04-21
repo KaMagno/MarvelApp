7d6b0d9f37ae20aa4ec013dc6838af7d377b5f40
Kaique Magno <kaique.magno@hotmail.com>
2018-04-21 15:34:06 -0300

    Created project structure

---
+++ XPInvestimento/CharacterDetailInteractor.swift
@@ -0,0 +1,13 @@
+//
+//  CharacterDetailInteractor.swift
+//  XPInvestimento
+//
+//  Created by Kaique Magno Dos Santos on 21/04/18.
+//  Copyright © 2018 Kaique Magno. All rights reserved.
+//
+
+import UIKit
+
+class CharacterDetailInteractor: NSObject {
+
+}
