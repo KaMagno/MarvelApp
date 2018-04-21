e64c495e3d47a3d8339c96c04e931f4400b53038
Kaique Magno <kaique.magno@hotmail.com>
2018-04-21 13:24:01 -0300

    Initial Commit

---
+++ XPInvestimento/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XPInvestimento
+//
+//  Created by Kaique Magno Dos Santos on 21/04/18.
+//  Copyright © 2018 Kaique Magno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
