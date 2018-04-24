0ffc6529fb84345985475f0910175b4bde6cc0ae
Kaique Magno <kaique.magno@hotmail.com>
2018-04-24 20:25:34 -0300

    - Created AlertModule - Implemented AlertModule

---
+++ XPInvestimento/AlertPresenter.swift
@@ -0,0 +1,44 @@
+//
+//  AlertPresenter.swift
+//  XPInvestimento
+//
+//  Created by Kaique Magno Dos Santos on 24/04/18.
+//  Copyright © 2018 Kaique Magno. All rights reserved.
+//
+
+import UIKit
+
+class AlertPresenter: NSObject {
+    
+    // MARK: - Variables
+    // MARK: Private
+    // MARK: Public
+    var router:AlertRouter
+    var interactor:AlertInteractor
+    var view:AlertViewController
+    
+    // MARK: - Initializers
+    init(router:AlertRouter,interactor:AlertInteractor,view:AlertViewController) {
+        self.router = router
+        self.interactor = interactor
+        self.view = view
+        
+        super.init()
+        self.view.presenter = self
+    }
+    
+    // MARK: - Functions
+    // MARK: Private
+    // MARK: Public
+    func viewDidLoad() {
+        self.view.set(message: self.interactor.message)
+        self.view.set(buttonTitle: self.interactor.buttonTitle)
+    }
+    
+    func didTapCancel() {
+        
+    }
+    func didTapConfirm() {
+        
+    }
+}
---
+++ XPInvestimento/AlertViewController.swift
@@ -0,0 +1,63 @@
+//
+//  AlertViewController.swift
+//  XPInvestimento
+//
+//  Created by Kaique Magno Dos Santos on 24/04/18.
+//  Copyright © 2018 Kaique Magno. All rights reserved.
+//
+
+import UIKit
+
+class AlertViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var outletAlertView: UIView!
+    @IBOutlet weak var outletMessageLabel: UILabel!
+    @IBOutlet weak var outletConfirmButton: UIButton!
+    
+    // MARK: - Properties
+    // MARK: Private
+    // MARK: Public
+    var presenter:AlertPresenter!
+    
+    // MARK: - UIViewController
+    // MARK: Functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.presenter.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: - Functions
+    // MARK: Private
+    private func setupView() {
+        self.outletAlertView.layer.borderColor = UIColor.black.cgColor
+        self.outletAlertView.layer.borderWidth = 2.0
+        
+        let tap = UITapGestureRecognizer.init(target: self, action: #selector(AlertViewController.handlerTapGesture(_:)))
+        self.view.addGestureRecognizer(tap)
+    }
+    // MARK: Public
+    func set(message:String) {
+        self.outletMessageLabel.text = message
+    }
+    
+    func set(buttonTitle:String) {
+        self.outletConfirmButton.setTitle(buttonTitle, for: .normal)
+    }
+    
+    @objc func handlerTapGesture(_ tapGestureRecognizer:UITapGestureRecognizer) {
+        self.presenter.didTapCancel()
+    }
+    
+    // MARK: - IBActions
+    @IBAction func tapCancelButton(_ sender: UIButton) {
+        self.presenter.didTapCancel()
+    }
+    @IBAction func tapConfirmButton(_ sender: UIButton) {
+        self.presenter.didTapConfirm()
+    }
+}
