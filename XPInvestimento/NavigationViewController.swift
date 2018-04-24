99d44d22a34649ee6c428e4bcc2894a16ff5e14c
Kaique Magno <kaique.magno@hotmail.com>
2018-04-24 18:34:47 -0300

    - Modified Font in NavigationControllerModule

--- XPInvestimento/NavigationViewController.swift
+++ XPInvestimento/NavigationViewController.swift
@@ -9,24 +9,29 @@
 import UIKit
 
 class NavigationViewController: UINavigationController {
-
+    
     var presenter:NavigationPresenter!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         
         self.navigationBar.prefersLargeTitles = true
         self.navigationBar.barTintColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
         self.navigationBar.largeTitleTextAttributes = [
-            NSAttributedStringKey.font: UIFont(name: "Marvel-Bold", size: 60.0) ?? UIFont.systemFont(ofSize: 20.0),
+            NSAttributedStringKey.font: UIFont(name: "Marvel-Bold", size: 60.0) ?? UIFont.systemFont(ofSize: 60.0),
+            NSAttributedStringKey.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        ]
+        self.navigationBar.titleTextAttributes = [
+            NSAttributedStringKey.font: UIFont(name: "Marvel-Bold", size: 20.0) ?? UIFont.systemFont(ofSize: 20.0),
             NSAttributedStringKey.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         ]
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 }
+
