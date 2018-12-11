ff4b885faa0a92d45085383a534c1bd935dcf00e
Kaique Magno <kaique.magno@hotmail.com>
2018-12-11 12:37:50 -0200

    Add an alert to CoreData errors instead of FatalError

--- MarvelApp/CoreDataSingleton.swift
+++ MarvelApp/CoreDataSingleton.swift
@@ -48,7 +48,7 @@ class CoreDataSingleton {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                AlertRouter.showAlert(with: "Error because Thanos is causing some trouble here, sorry. We call our developer team to work with the problem")
             }
         }
     }
